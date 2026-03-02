e53a21fb65af43607a12d9b6e52285c5f337d671
porta <llportai92@alumnes.ub.edu>
2026-03-02 10:33:43 +0100

    Daniel Ortiz goat

--- src/prog2/model/Allotjament.java
+++ src/prog2/model/Allotjament.java
@@ -1,4 +1,40 @@
 package prog2.model;
 
-public class Allotjament {
-}
+public abstract class Allotjament implements InAllotjament {
+    private String nom;
+    private String Id;
+    private long EstadaMinima;
+    private enum Temp {ALTA, BAIXA};
+
+    public String getNom(){
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getId(){
+        return Id;
+    }
+
+    public void setId(String Id){
+        this.Id = Id;
+    }
+
+    public long getEstadaMinima(Temp temp){
+        if (temp == Temp.ALTA) {
+            return EstadaMinima;
+        }else {
+            return EstadaMinima;
+        }
+    }
+
+    public void setEstadaMinima(long estadaMinimaALTA_, long estadaMinimaBAIXA_) {
+        Temp.ALTA = estadaMinimaALTA_;
+        Temp.BAIXA = estadaMinimaBAIXA_;
+    }
+
+    public abstract boolean correcteFuncionament();
+
+
+
--- src/prog2/model/Reserva.java
+++ src/prog2/model/Reserva.java
@@ -1,4 +1,32 @@
 package prog2.model;
 
+import java.time.LocalDate;
+
 public class Reserva {
+    private Allotjament allotjament;
+    private Client client;
+    private LocalDate dataEntrada;
+    private LocalDate dataSortida;
+
+    public Reserva(String id_, String dni_, LocalDate dataEntrada, LocalDate dataSortida){
+        Allotjament.setId(id_);
+        Client.setDni(dni_);
+        this.dataEntrada = dataEntrada;
+        this.dataSortida = dataSortida;
+    }
+    public Allotjament getAllotjament_();
+
+    public Client getClient();
+
+    public LocalDate getDataEntrada();
+
+    public LocalDate getDataSortida();
+
+    public void setAllotjament_(Allotjament allotjament_);
+
+    public void setClient(Client client_);
+
+    public void setDataEntrada(LocalDate dataEntrada_);
+
+    public void setDataSortida(LocalDate dataSortida_);
 }
